a96ec86354f9f9408355fd66613e0ae264ea7da7
Andrew Edmondson <ed@rameus.org.uk>
2011-09-27 17:26:23 +0100

    Working on the admin interface

--- PyNetCCTVDjangoManager/admin.py
+++ PyNetCCTVDjangoManager/admin.py
@@ -1,8 +1,8 @@
-from PyNetCCTVDjangoManager.models import Camera
+from PyNetCCTVDjangoManager.models import Camera, Snapshot
 from django.contrib import admin
 
 class CameraAdmin(admin.ModelAdmin):
     list_display=["name","interval"]
 
 admin.site.register(Camera, CameraAdmin)
-
+admin.site.register(Snapshot)
