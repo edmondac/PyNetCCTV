b131f5e6a81f81033829e9fbf6bb6c928f616a69
Andrew Edmondson <ed@rameus.org.uk>
2011-09-27 17:23:07 +0100

    Not using that yet...

--- SnapshotDaemon/Camera_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import Snapshot
-from ../PyNetCCTVDjangoManager.models import Camera as DjangoCamera
-
-class CameraTestCase(unittest.TestCase):
-    def setUp(self):
-        self.dj_cam1 = DjangoCamera(name="test1",
-                                    username="user",
-                                    
