4411cc03062e3c274c97680b3af828dae9f6395e
Andrew Edmondson <ed@rameus.org.uk>
2011-09-30 10:31:05 +0100

    Datetime filtering works on snapshots page

--- PyNetCCTVDjangoManager/views.py
+++ PyNetCCTVDjangoManager/views.py
@@ -2,12 +2,15 @@ from PyNetCCTVDjangoManager.models import Camera, Snapshot
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-import math
+import math, datetime
 
 def index(request):
     n_cams = Camera.objects.count()
     n_snaps = Snapshot.objects.count()
-    return render_to_response('cctv/index.html', {'cameras':n_cams, 'snapshots': n_snaps}, context_instance=RequestContext(request))
+    return render_to_response('cctv/index.html',
+                              {'cameras':n_cams,
+                               'snapshots': n_snaps},
+                              context_instance=RequestContext(request))
 
 def montage(request):
     cams = Camera.objects.all()
@@ -44,8 +47,26 @@ def snapshot(request):
 
 def snapshots(request):
     cams = Camera.objects.all()
-    #NOTE: all() is lazy, so don't worry about scale problems here with pagination
-    snaps = Snapshot.objects.all().order_by('-timestamp')
+
+    from_date = request.GET.get('from_date')
+    from_time = request.GET.get('from_time',"00:00")
+    to_date = request.GET.get('to_date')
+    to_time = request.GET.get('to_time',"00:00")
+
+    try:
+        from_obj = datetime.datetime.strptime("%s %s" %(from_date, from_time), "%d/%m/%y %H:%M")
+        to_obj = datetime.datetime.strptime("%s %s" %(to_date, to_time), "%d/%m/%y %H:%M")
+    except ValueError:
+        from_obj = None
+        to_obj = None
+
+    if from_obj and to_obj:
+        snaps = Snapshot.objects.filter(timestamp__range=(from_obj, to_obj))
+    else:
+        snaps = Snapshot.objects.all().order_by('-timestamp')
+        
+    #NOTE: QuerySets are generally lazy, so don't worry
+    #      about scale problems here with pagination
     number = request.GET.get('per_page',25)
     page = request.GET.get('page',1)
     
@@ -71,6 +92,10 @@ def snapshots(request):
     return render_to_response('cctv/snapshots.html', 
                               {'cameras':cams,
                                'objects':objects,
-                               'page':page}, 
+                               'page':page,
+                               'from_date':from_date,
+                               'from_time':from_time,
+                               'to_date':to_date,
+                               'to_time':to_time}, 
                               context_instance=RequestContext(request))
 
